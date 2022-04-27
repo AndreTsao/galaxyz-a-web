625bac2a7197a17e361d23c1494c805b6b341e96
JetCyC <670182161@qq.com>
2022-04-27 22:30:12 +0800

    ui

--- components/About.js
+++ components/About.js
@@ -30,12 +30,11 @@ function About() {
         style={{ marginBottom: '20px', color: 'white' }}
         variant="body1"
         gutterBottom>
-        TocaIsland is an Multi-Blockchain Metaverse island. The ecosystem in TocaIsland
-         is mainly composed of TocaboNFT(Bigmouth bird)🐧, TocaTreeNFT🌴 and $TocaWorm Token🪱.
-        A Tocabo will get a piece of TocaIsland land for free in Q4 2022! The stronger a Tocabo,the more territory it has in TocaIsland.
-        TocaTree produces $TocaWorm Token.The more $TocaWorm a Tocabo eats, the stronger it will become.
+        TocaIsland is an Multi-Blockchain Metaverse island. The ecosystem in TocaIsland is mainly composed of 
+        TocaboNFT(Bigmouth bird)🐧, TocaTreeNFT🌴 and $TocaWorm Token🪱. A Tocabo will get a piece of TocaIsland land for free in Q4 2022! 
+        <br/>The stronger a Tocabo,the more territory it has in TocaIsland. TocaTree produces $TocaWorm Token.The more $TocaWorm a Tocabo eats, the stronger it will become.
         TocaboNFT is an exclusive NFT collection of 5666 toucans NFT and it is the PASSCARD into TocaIsland.
-        TocaTreeNFT is an exclusive NFT collection of 5666 many kinds of trees NFT.
+        <br/>TocaTreeNFT is an exclusive NFT collection of 5666 many kinds of trees NFT.
         The total supply of $TocaWorm Token is 1,000,000,000.
         TocaIsland covers 50,000 acres.
       </Typography>
